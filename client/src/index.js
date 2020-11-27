9910b3e25999666362952354cb7033bc2226656a
Alexey Onikov <orbenaixasej@gmail.com>
2020-11-27 11:41:12 +0300

    Updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+});
+
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App/>
+  </ThemeProvider>,
   document.getElementById('root')
 );
